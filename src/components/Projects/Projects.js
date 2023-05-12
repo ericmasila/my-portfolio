48c5a093391739a444868ebb3a249fba0b4528d3
ericmasila <ericmasila254@gmail.com>
2023-05-12 11:30:44 +0300

    git commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,7 +23,7 @@ function Projects() {
               imgPath={leaf}
               isBlog={false}
               title="Number-Plate-Capturing"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              description="Designed and trained this ML model for a project to capture, analyze and store number plates of moving cars. The model was successful in capturing but experienced problems in analyzing."
               ghLink="https://github.com/ericmasila/number-plate-capturing"
               demoLink="https://plant49-ai.herokuapp.com/"
             />
